efae80d4511adaab3c1ffe9ac6939534c2ea9e56
theberserker <andrej.spilak@gmail.com>
2015-07-22 12:27:58 +0200

    Default project

---
+++ SignalRTestDrive.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRTestDrive.MVC.MvcApplication" Language="C#" %>
---
+++ SignalRTestDrive.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRTestDrive.MVC.Startup))]
+namespace SignalRTestDrive.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
